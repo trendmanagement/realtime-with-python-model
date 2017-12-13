16f25650561d526ad3ce641b4a4f6f864ce997a6
spickering@outlook.com <NJoyce111174$>
2017-12-13 12:19:19 -0800

    added

---
+++ RealtimeSpreadMonitor/RealtimeSpreadMonitor/Forms/FCM_ReportSummaryForm.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Globalization;
+using RealtimeSpreadMonitor.Model;
+
+namespace RealtimeSpreadMonitor.Forms
+{
+    public partial class FCM_ReportSummaryForm : Form
+    {
+        delegate void ThreadSafeHideUnhideContractSummaryLiveDataDelegate(int row, bool visible);
+
+        //private OptionStrategy[] optionStrategies;
+        //private Instrument_mongo[] instruments;
+
+        //private OptionSpreadManager optionSpreadManager;
+
+        private List<ADMPositionImportWeb> admSummaryFieldsList;
+
+        //private ADM_IMPORT_FILE_TYPES fileImportType;
+
+        private long instrumentSelectedIdx;
+
+        public FCM_ReportSummaryForm()
+        {
+            InitializeComponent();
+            
+            //this.optionStrategies = optionStrategies;
+            //this.instruments = instrumentArray;
+            
+        }
+
+
+
+        public void fillFileDateTimeLabel(DateTime fileDateTime)
+        {
+            StringBuilder lastFileWriteTime = new StringBuilder();
+
+            lastFileWriteTime.Append("ADM FILE LAST LOADED ");
+
+            lastFileWriteTime.Append(
+                fileDateTime.ToString("MMM dd yyyy HH:mm:ss", DateTimeFormatInfo.InvariantInfo));
+
+            lblLatestADMFileDate.Text = lastFileWriteTime.ToString();
+                
+        }
+
+        public void setupAdmInputSummaryGrid(List<ADMPositionImportWeb> admSummaryFieldsList)
+        {
+#if DEBUG
+            try
+#endif
+            {
+                
+
+                this.admSummaryFieldsList = admSummaryFieldsList;
+
+                fcm_InputSummaryGrid.DataSource = FCM_DataImportLibrary.FCM_fullImportDataTableForDisplay;
+
+                Type admSummaryFieldDisplayedTypes = typeof(ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED);
+                Array admSummaryFieldDisplayedTypeArray = Enum.GetNames(admSummaryFieldDisplayedTypes);
+
+                //admInputSummaryGrid.ColumnCount = admSummaryFieldDisplayedTypeArray.Length;  // admSummaryFieldTypeArray.Length;
+
+                //admInputSummaryGrid.EnableHeadersVisualStyles = false;
+
+                //DataGridViewCellStyle colTotalPortStyle = admInputSummaryGrid.ColumnHeadersDefaultCellStyle;
+                //colTotalPortStyle.BackColor = Color.Black;
+                //colTotalPortStyle.ForeColor = Color.White;
+
+                //DataGridViewCellStyle rowTotalPortStyle = admInputSummaryGrid.RowHeadersDefaultCellStyle;
+                //rowTotalPortStyle.BackColor = Color.Black;
+                //rowTotalPortStyle.ForeColor = Color.White;
+
+                //for (int i = 0; i < admInputSummaryGrid.ColumnCount; i++)
+                //{
+                //    admInputSummaryGrid.Columns[i].SortMode = DataGridViewColumnSortMode.NotSortable;
+                //    admInputSummaryGrid.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                //}
+
+                //int col = 0;
+
+                if (FCM_DataImportLibrary.FCM_fullImportDataTableForDisplay.Columns.Count == 0)
+                {
+
+                    for (int admCount = 0; admCount < admSummaryFieldDisplayedTypeArray.Length; admCount++)
+                    {
+
+
+                        FCM_DataImportLibrary.FCM_fullImportDataTableForDisplay.Columns.Add(
+                            admSummaryFieldDisplayedTypeArray.GetValue(admCount).ToString().Replace('_', ' '));
+                    }
+
+                }
+
+
+
+                //admInputSummaryGrid.RowCount = admSummaryFieldsList.Count;
+
+                //int col = 0;
+
+
+
+                //for (int admRowCounter = 0; admRowCounter < admSummaryFieldsList.Count; admRowCounter++)
+                //{
+                //    admInputSummaryGrid.Rows[admRowCounter].Cells[(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.RecordType].Value
+                //        = admSummaryFieldsList[admRowCounter].RecordType;
+
+
+                //    admInputSummaryGrid.Rows[admRowCounter].Cells[(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.Office].Value
+                //        = admSummaryFieldsList[admRowCounter].POFFIC;
+
+                //    admInputSummaryGrid.Rows[admRowCounter].Cells[(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.Acct].Value
+                //        = admSummaryFieldsList[admRowCounter].PACCT;
+
+
+                //    admInputSummaryGrid.Rows[admRowCounter].Cells[(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.Description].Value
+                //        = admSummaryFieldsList[admRowCounter].Description;
+
+                //    admInputSummaryGrid.Rows[admRowCounter].Cells[(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.LongQuantity].Value
+                //        = (int)admSummaryFieldsList[admRowCounter].LongQuantity;
+
+                //    admInputSummaryGrid.Rows[admRowCounter].Cells[(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.ShortQuantity].Value
+                //        = (int)admSummaryFieldsList[admRowCounter].ShortQuantity;
+
+
+                //    admInputSummaryGrid.Rows[admRowCounter].Cells[(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.TradeDate].Value
+                //        = admSummaryFieldsList[admRowCounter].TradeDate;
+
+                //    admInputSummaryGrid.Rows[admRowCounter].Cells[(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.TradePrice].Value
+                //        = admSummaryFieldsList[admRowCounter].TradePrice;
+
+                //    admInputSummaryGrid.Rows[admRowCounter].Cells[(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.WeightedPrice].Value
+                //        = admSummaryFieldsList[admRowCounter].WeightedPrice;
+
+
+                //    admInputSummaryGrid.Rows[admRowCounter].Cells[(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.SettledPrice].Value
+                //        = admSummaryFieldsList[admRowCounter].SettledPrice;
+
+
+                //    admInputSummaryGrid.Rows[admRowCounter].Cells[(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.SettledValue].Value
+                //        = admSummaryFieldsList[admRowCounter].SettledValue;
+
+                //    admInputSummaryGrid.Rows[admRowCounter].Cells[(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.Currency].Value
+                //        = admSummaryFieldsList[admRowCounter].Currency;
+
+                //    //                     admInputSummaryGrid.Rows[admRowCounter].Cells[(int)ADM_SUMMARY_FIELDS.PSUBTY].Value
+                //    //                         = admSummaryFieldsList[admRowCounter].PSUBTY;
+
+                //    //                     admInputSummaryGrid.Rows[admRowCounter].Cells[(int)ADM_SUMMARY_FIELDS.PEXCH].Value
+                //    //                         = admSummaryFieldsList[admRowCounter].PEXCH;
+
+                //    admInputSummaryGrid.Rows[admRowCounter].Cells[(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.PFC].Value
+                //        = admSummaryFieldsList[admRowCounter].PFC;
+
+                //    admInputSummaryGrid.Rows[admRowCounter].Cells[(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.Strike].Value
+                //        = admSummaryFieldsList[admRowCounter].aDMStrike;
+
+                //    admInputSummaryGrid.Rows[admRowCounter].Cells[(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.PCTYM].Value
+                //        = admSummaryFieldsList[admRowCounter].PCTYM;
+
+                //    admInputSummaryGrid.Rows[admRowCounter].Cells[(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.INSTRUMENT_ID].Value
+                //        = admSummaryFieldsList[admRowCounter].idinstrument;
+                //}
+
+                //                updateSelectedInstrumentFromTreeADM(instrumentSelectedIdx);
+
+                fillFCMData();
+            }
+#if DEBUG
+            catch (Exception ex)
+            {
+                TSErrorCatch.errorCatchOut(Convert.ToString(this), ex);
+            }
+#endif
+        }
+
+        internal void fillFCMData()
+        {
+            DataTable dataTable = FCM_DataImportLibrary.FCM_fullImportDataTableForDisplay;
+
+            dataTable.Rows.Clear();
+
+            int rowCount = 0;
+
+            foreach (ADMPositionImportWeb fcmPosImport in admSummaryFieldsList)
+            {
+                if (fcmPosImport.instrument.idinstrument == DataCollectionLibrary.instrumentSelectedInTreeGui
+                                || DataCollectionLibrary.instrumentSelectedInTreeGui == TradingSystemConstants.ALL_INSTRUMENTS_SELECTED)
+                {
+                    //AccountAllocation ac =
+                    //    DataCollectionLibrary.portfolioAllocation.accountAllocation_KeyAccountname[ap.name];
+
+                    if (fcmPosImport.acctGroup.visible)
+                    {
+
+                        dataTable.Rows.Add();
+
+                        rowCount = dataTable.Rows.Count - 1;
+
+                        dataTable.Rows[rowCount][(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.RecordType]
+                            = fcmPosImport.RecordType;
+
+
+                        dataTable.Rows[rowCount][(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.Office]
+                            = fcmPosImport.POFFIC;
+
+                        dataTable.Rows[rowCount][(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.Acct]
+                            = fcmPosImport.PACCT;
+
+
+                        dataTable.Rows[rowCount][(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.Description]
+                            = fcmPosImport.Description;
+
+                        dataTable.Rows[rowCount][(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.LongQuantity]
+                            = (int)fcmPosImport.LongQuantity;
+
+                        dataTable.Rows[rowCount][(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.ShortQuantity]
+                            = (int)fcmPosImport.ShortQuantity;
+
+
+                        dataTable.Rows[rowCount][(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.TradeDate]
+                            = fcmPosImport.TradeDate;
+
+                        dataTable.Rows[rowCount][(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.TradePrice]
+                            = fcmPosImport.TradePrice;
+
+                        dataTable.Rows[rowCount][(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.WeightedPrice]
+                            = fcmPosImport.WeightedPrice;
+
+
+                        dataTable.Rows[rowCount][(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.SettledPrice]
+                            = fcmPosImport.SettledPrice;
+
+
+                        dataTable.Rows[rowCount][(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.SettledValue]
+                            = fcmPosImport.SettledValue;
+
+                        dataTable.Rows[rowCount][(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.Currency]
+                            = fcmPosImport.Currency;
+
+                        //                     dataTable.Rows[admRowCounter][(int)ADM_SUMMARY_FIELDS.PSUBTY]
+                        //                         = fcmPosImport.PSUBTY;
+
+                        //                     dataTable.Rows[admRowCounter][(int)ADM_SUMMARY_FIELDS.PEXCH]
+                        //                         = fcmPosImport.PEXCH;
+
+                        dataTable.Rows[rowCount][(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.PFC]
+                            = fcmPosImport.PFC;
+
+                        dataTable.Rows[rowCount][(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.Strike]
+                            = fcmPosImport.aDMStrike;
+
+                        dataTable.Rows[rowCount][(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.PCTYM]
+                            = fcmPosImport.PCTYM;
+
+                        dataTable.Rows[rowCount][(int)ADM_SUMMARY_WEB_POSITION_FIELDS_DISPLAYED.INSTRUMENT_ID]
+                            = fcmPosImport.idinstrument;
+                    }
+                }
+            }
+        }
+
+        private void AdmReportSummaryForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+#if DEBUG
+            try
+#endif
+            {
+                e.Cancel = true;
+                this.Visible = false;
+            }
+#if DEBUG
+            catch (Exception ex)
+            {
+                TSErrorCatch.errorCatchOut(Convert.ToString(this), ex);
+            }
+#endif
+        }
+
+        private void AdmReportSummaryForm_DragEnter(object sender, DragEventArgs e)
+        {
+#if DEBUG
+            try
+#endif
+            {
+                e.Effect = DragDropEffects.All;
+            }
+#if DEBUG
+            catch (Exception ex)
+            {
+                TSErrorCatch.errorCatchOut(Convert.ToString(this), ex);
+            }
+#endif
+        }
+
+
+
+        
+
+
+        private void threadSafeDisplayADMInputWithWebPositions(OptionSpreadManager optionSpreadManager,
+            int contractSummaryInstrumentSelectedIdx)
+        {
+            this.Show();
+
+            this.BringToFront();
+
+            setupAdmInputSummaryGrid(FCM_DataImportLibrary.FCM_positionImportNotConsolidated);
+
+        }
+    }
+}
---
+++ RealtimeSpreadMonitor/RealtimeSpreadMonitor/Forms/FCM_ReportSummaryForm.designer.cs
@@ -0,0 +1,135 @@
+﻿namespace RealtimeSpreadMonitor.Forms
+{
+    partial class FCM_ReportSummaryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FCM_ReportSummaryForm));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.lblLatestADMFileDate = new System.Windows.Forms.Label();
+            this.fcm_InputSummaryGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fcm_InputSummaryGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.lblLatestADMFileDate);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.fcm_InputSummaryGrid);
+            this.splitContainer1.Size = new System.Drawing.Size(801, 545);
+            this.splitContainer1.SplitterDistance = 29;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // lblLatestADMFileDate
+            // 
+            this.lblLatestADMFileDate.AutoSize = true;
+            this.lblLatestADMFileDate.Location = new System.Drawing.Point(12, 9);
+            this.lblLatestADMFileDate.Name = "lblLatestADMFileDate";
+            this.lblLatestADMFileDate.Size = new System.Drawing.Size(109, 13);
+            this.lblLatestADMFileDate.TabIndex = 0;
+            this.lblLatestADMFileDate.Text = "lblLatestADMFileDate";
+            // 
+            // fcm_InputSummaryGrid
+            // 
+            this.fcm_InputSummaryGrid.AllowUserToAddRows = false;
+            this.fcm_InputSummaryGrid.AllowUserToDeleteRows = false;
+            this.fcm_InputSummaryGrid.AllowUserToResizeRows = false;
+            this.fcm_InputSummaryGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.fcm_InputSummaryGrid.BackgroundColor = System.Drawing.Color.White;
+            this.fcm_InputSummaryGrid.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.fcm_InputSummaryGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.fcm_InputSummaryGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.fcm_InputSummaryGrid.DefaultCellStyle = dataGridViewCellStyle2;
+            this.fcm_InputSummaryGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.fcm_InputSummaryGrid.Location = new System.Drawing.Point(0, 0);
+            this.fcm_InputSummaryGrid.Name = "fcm_InputSummaryGrid";
+            this.fcm_InputSummaryGrid.ReadOnly = true;
+            this.fcm_InputSummaryGrid.RowHeadersVisible = false;
+            this.fcm_InputSummaryGrid.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToDisplayedHeaders;
+            this.fcm_InputSummaryGrid.Size = new System.Drawing.Size(801, 512);
+            this.fcm_InputSummaryGrid.TabIndex = 7;
+            // 
+            // FCM_ReportSummaryForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(801, 545);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FCM_ReportSummaryForm";
+            this.Text = "AdmReportSummaryForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.AdmReportSummaryForm_FormClosing);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.AdmReportSummaryForm_DragEnter);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.fcm_InputSummaryGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label lblLatestADMFileDate;
+        private System.Windows.Forms.DataGridView fcm_InputSummaryGrid;
+    }
+}
